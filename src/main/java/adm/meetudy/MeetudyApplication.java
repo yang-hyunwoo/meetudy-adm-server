101e71e0199fe3a0ece4a9a10bd517522daaa73d
cohouseol <cohouseol@gmail.com>
2025-04-22 22:52:13 +0900

    프로젝트 init

---
+++ src/main/java/adm/meetudy/MeetudyApplication.java
@@ -0,0 +1,13 @@
+package adm.meetudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetudyApplication.class, args);
+	}
+
+}
